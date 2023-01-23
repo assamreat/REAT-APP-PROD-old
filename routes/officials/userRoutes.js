af29ef038ad210e25a7b0a2a816568e5f1eb9792
Bidyut Deka <bidyutranjan464@gmail.com>
2023-01-23 11:44:38 +0530

    admin creation path commented out

--- routes/officials/userRoutes.js
+++ routes/officials/userRoutes.js
@@ -17,89 +17,89 @@ const Appellant = require('../../models/Appellant');
 // @desc  Create Admin - needs to be deleted
 // @access Public
 
-router.post(
-    '/admin',
-    [
-        body('firstName', 'Please enter a first name').isLength({ min: 1 }),
-        body('lastName', 'Please enter a last name').isLength({ min: 1 }),
-        body('email', 'Please include a valid email').isEmail(),
-        body(
-            'password',
-            'Please enter a password with 6 or more character'
-        ).isLength({
-            min: 6,
-        }),
-        body('role', 'Please include an user role').isLength({ min: 1 }),
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
+// router.post(
+//     '/admin',
+//     [
+//         body('firstName', 'Please enter a first name').isLength({ min: 1 }),
+//         body('lastName', 'Please enter a last name').isLength({ min: 1 }),
+//         body('email', 'Please include a valid email').isEmail(),
+//         body(
+//             'password',
+//             'Please enter a password with 6 or more character'
+//         ).isLength({
+//             min: 6,
+//         }),
+//         body('role', 'Please include an user role').isLength({ min: 1 }),
+//     ],
+//     async (req, res) => {
+//         const errors = validationResult(req);
 
-        if (!errors.isEmpty()) {
-            // return res.status(400).json({ errors: errors.array() });
+//         if (!errors.isEmpty()) {
+//             // return res.status(400).json({ errors: errors.array() });
 
-            let errObj = {};
-            errors.array().map((error) => {
-                errObj[error.param] = error.msg;
-            });
-            return res.status(400).json(errObj);
-        }
-        const { firstName, lastName, email, password, role } = req.body;
+//             let errObj = {};
+//             errors.array().map((error) => {
+//                 errObj[error.param] = error.msg;
+//             });
+//             return res.status(400).json(errObj);
+//         }
+//         const { firstName, lastName, email, password, role } = req.body;
 
-        try {
-            // See if user exists
-            const existingUser = await User.findOne({
-                where: { email: email },
-            });
+//         try {
+//             // See if user exists
+//             const existingUser = await User.findOne({
+//                 where: { email: email },
+//             });
 
-            if (existingUser) {
-                return res.status(400).json({
-                    email: 'User already exists',
-                });
-            }
+//             if (existingUser) {
+//                 return res.status(400).json({
+//                     email: 'User already exists',
+//                 });
+//             }
 
-            // Create a new User
-            const user = User.build({
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                password: password,
-                role: role,
-                active: 1,
-            });
+//             // Create a new User
+//             const user = User.build({
+//                 firstName: firstName,
+//                 lastName: lastName,
+//                 email: email,
+//                 password: password,
+//                 role: role,
+//                 active: 1,
+//             });
 
-            // generate the salt
-            const salt = await bcrypt.genSalt(10);
+//             // generate the salt
+//             const salt = await bcrypt.genSalt(10);
 
-            // hash the password using bcryptjs
-            user.password = await bcrypt.hash(password, salt);
+//             // hash the password using bcryptjs
+//             user.password = await bcrypt.hash(password, salt);
 
-            // save the user
-            await user.save();
+//             // save the user
+//             await user.save();
 
-            // Return jsonwebtoken
-            const payload = {
-                user: {
-                    id: `${keys.jwtPayload}${user.id}`,
-                    // id: user.id,
-                    type: 'GOVT',
-                },
-            };
+//             // Return jsonwebtoken
+//             const payload = {
+//                 user: {
+//                     id: `${keys.jwtPayload}${user.id}`,
+//                     // id: user.id,
+//                     type: 'GOVT',
+//                 },
+//             };
 
-            jwt.sign(
-                payload,
-                keys.jwtSecret,
-                { expiresIn: 360000 },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({ token });
-                }
-            );
-        } catch (err) {
-            console.log(err.message);
-            res.status(500).send('Server Error');
-        }
-    }
-);
+//             jwt.sign(
+//                 payload,
+//                 keys.jwtSecret,
+//                 { expiresIn: 360000 },
+//                 (err, token) => {
+//                     if (err) throw err;
+//                     res.json({ token });
+//                 }
+//             );
+//         } catch (err) {
+//             console.log(err.message);
+//             res.status(500).send('Server Error');
+//         }
+//     }
+// );
 
 // @route POST api/users/
 // @desc  create new user
